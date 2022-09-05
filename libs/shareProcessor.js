f0006eae6519da8052e342b10f842578ca04474b
Shat <shatll_s@mail.ru>
2022-09-05 17:07:20 +0300

    Added block reward and effort to database. Added "database" option to config

--- libs/shareProcessor.js
+++ libs/shareProcessor.js
@@ -1,7 +1,7 @@
 var redis = require('redis');
 var Stratum = require('stratum-pool');
 var CreateRedisClient = require('./createRedisClient.js');
-
+var async = require('async');
 
 
 /*
@@ -14,10 +14,7 @@ value: a hash with..
 
  */
 
-
-
 module.exports = function(logger, poolConfig){
-
     var redisConfig = poolConfig.redis;
     var coin = poolConfig.coin.name;
 
@@ -66,8 +63,79 @@ module.exports = function(logger, poolConfig){
         }
     });
 
-    this.handleShare = function(isValidShare, isValidBlock, shareData) {
+    var poolConfigs = JSON.parse(process.env.pools);
+    var poolOptions = poolConfigs[coin];
+    var processingConfig = poolOptions.paymentProcessing;
+    var daemon = new Stratum.daemon.interface([processingConfig.daemon], function(severity, message){
+        logger[severity](logSystem, logComponent, message);
+    });
 
+    function getReward(txHash, callback){
+        daemon.cmd('gettransaction', [txHash], function (result) {
+            if (!result || result[0].error) {
+                callback(result[0].error, null);
+                return;
+            }
+            if (!result[0].response
+                || !result[0].response.details
+                || !result[0].response.details[0]
+                || !result[0].response.details[0].amount) {
+                callback('No response or no details in response', null);
+                return;
+            }
+
+            let fee = 0;
+            if (poolOptions.rewardRecipients) {
+                for (let key of Object.keys(poolOptions.rewardRecipients)) {
+                    fee += +poolOptions.rewardRecipients[key];
+                }
+            }
+            callback(null, result[0].response.details[0].amount * 100 / (100 - fee));
+            return;
+        });
+    }
+
+    function getDifficulty(callback){
+        daemon.cmd('getdifficulty', [], function (result) {
+            if (!result || result[0].error) {
+                callback(result[0].error, null);
+                return;
+            }
+            if (!result[0].response) {
+                callback('No response or no details in response', null);
+                return;
+            }
+
+            callback(null, result[0].response);
+            return;
+        });
+    }
+
+    function getShares(callback){
+        var localRedisCommands = [
+            ['hgetall', coin + ':shares:roundCurrent']
+        ]
+        connection.multi(localRedisCommands).exec(function (err, result) {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+            if (!result || !result[0]) {
+                callback('No response or no details in response', null);
+                return;
+            }
+
+            let roundDiff = 0;
+            for (let key of Object.keys(result[0])) {
+                roundDiff += +result[0][key];
+            }
+            callback(null, roundDiff);
+            return;
+        });
+
+    }
+
+    this.handleShare = function(isValidShare, isValidBlock, shareData) {
         var redisCommands = [];
 
         if (isValidShare) {
@@ -87,17 +155,50 @@ module.exports = function(logger, poolConfig){
         if (isValidBlock){
             redisCommands.push(['rename', coin + ':shares:roundCurrent', coin + ':shares:round' + shareData.height]);
             redisCommands.push(['rename', coin + ':shares:timesCurrent', coin + ':shares:times' + shareData.height]);
-            redisCommands.push(['sadd', coin + ':blocksPending', [shareData.blockHash, shareData.txHash, shareData.height, shareData.worker, dateNow].join(':')]);
+            // redisCommands.push(['sadd', coin + ':blocksPending', [shareData.blockHash, shareData.txHash, shareData.height, shareData.worker, dateNow].join(':')]);
             redisCommands.push(['hincrby', coin + ':stats', 'validBlocks', 1]);
-        }
-        else if (shareData.blockHash){
-            redisCommands.push(['hincrby', coin + ':stats', 'invalidBlocks', 1]);
+
+            // console.log(`Found valid ${coin} block`, shareData)
+            async.parallel({
+                reward: ((cb) => {
+                    getReward(shareData.txHash, cb)
+                }),
+                difficulty: ((cb) => {
+                    getDifficulty(cb)
+                }),
+                shares: ((cb) => {
+                    getShares(cb)
+                }),
+            }, (err, res) => {
+                // console.log('err', err)
+                // console.log('res', res)
+                if (err) {
+                    logger.error(logSystem, logComponent, logSubCat, err);
+                    redisCommands.push(['sadd', coin + ':blocksPending', [shareData.blockHash, shareData.txHash, shareData.height, shareData.worker, dateNow].join(':')]);
+                } else {
+                    logger.debug(logSystem, logComponent, logSubCat, `Successfully requested reward for block ${shareData.blockHash}`);
+                    var blockEffort = Math.floor(res.shares / res.difficulty * Math.pow(10, 4)) / Math.pow(10, 4);
+                    redisCommands.push(['sadd', coin + ':blocksPending', [shareData.blockHash, shareData.txHash, shareData.height, shareData.worker, dateNow, res.reward, blockEffort].join(':')]);
+                }
+
+                // console.log('exec next commands', redisCommands)
+                connection.multi(redisCommands).exec(function (err, replies) {
+                    if (err)
+                        logger.error(logSystem, logComponent, logSubCat, 'Error with share processor multi ' + JSON.stringify(err));
+                });
+            });
+
+        } else {
+            if (shareData.blockHash) {
+                redisCommands.push(['hincrby', coin + ':stats', 'invalidBlocks', 1]);
+            }
+
+            connection.multi(redisCommands).exec(function (err, replies) {
+                if (err)
+                    logger.error(logSystem, logComponent, logSubCat, 'Error with share processor multi ' + JSON.stringify(err));
+            });
         }
 
-        connection.multi(redisCommands).exec(function(err, replies){
-            if (err)
-                logger.error(logSystem, logComponent, logSubCat, 'Error with share processor multi ' + JSON.stringify(err));
-        });
     };
 
 };
